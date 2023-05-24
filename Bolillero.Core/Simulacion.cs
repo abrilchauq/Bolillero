3660ea0adb425b24470820f2be3e6d1f91e66c26
abrilchauq <abril.chauqueet12d1@gmail.com>
2023-05-24 15:48:33 -0300

    Update Simulacion.cs

--- Bolillero.Core/Simulacion.cs
+++ Bolillero.Core/Simulacion.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Bolillero.Core
 {
     public class Simulacion
     {
-        
-    }
+        public long simularSinHilos(Bolilla bolillero, List<int> jugada, int cantidad) 
+            => bolillero.JugarNVeces(jugada, cantidad);
+
+        public long simularConHilos(Bolilla bolillero, List<int> jugada, int cantidad)
+        {
+            Task<long>
+        }
+        // declarar vector tipo Task<long> implementar for
+    } 
 }
