ef61c4a34c7809ab9d9712ca91a918a8a711479a
abrilchauq <abril.chauqueet12d1@gmail.com>
2023-05-31 15:40:25 -0300

    Create SimulacionTest.cs

---
+++ BolilleroTest/SimulacionTest.cs
@@ -0,0 +1,13 @@
+namespace BolilleroTest
+{
+    public class SimulacionTest
+    {
+        public int MyProperty { get; set; }
+
+        [Fact]
+        public void simularSinHilos()
+        {
+            Assert.True(true);
+        }
+    }
+}
