ee939b6258a236f6aeefa7977a8a73cf8c009d36
abrilchauq <abril.chauqueet12d1@gmail.com>
2023-06-12 17:11:24 -0300

    Update SimulacionTest.cs

--- BolilleroTest/SimulacionTest.cs
+++ BolilleroTest/SimulacionTest.cs
@@ -4,20 +4,32 @@ namespace BolilleroTest
 {
     public class SimulacionTest
     {
-        public Bolilla bolillero { get; set; }
-        public Simulacion simulacion { get; set; }
-        public List<int> jugada { get; set; }
-        public int cantidad { get; set; }
+        public Bolilla bolillero { get; }
+        public Simulacion simulacion { get; }
+        public List<int> jugada { get; }
+        public int cantidad { get; }
+        private int hilos { get; }
 
         public SimulacionTest()
         {
-            
+            this.bolillero = new Bolilla(10, new ElegirPrimera());
+            this.simulacion = new Simulacion();
+            this.jugada = new List<int>(){ 0 }; 
+            this.cantidad = 1;
+            this.hilos = 4;
         }
 
         [Fact]
         public void simularSinHilos()
         {
-            Assert.True(true);
+            var ganadas = simulacion.simularSinHilos(bolillero, jugada, cantidad);
+            Assert.Equal(1, ganadas);
+        }
+
+        [Fact]
+        public void simularConHilos()
+        {
+            
         }
     }
 }
